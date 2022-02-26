d3f8739168521a9e21017e778be3fb1509050056
Koba4444 <koba4444@gmail.com>
2022-02-26 17:00:14 +0300

    first commit
    Signed-off-by: Koba4444 <koba4444@gmail.com>

---
+++ c3w4t1env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\koba0\education\20211213_мфти_программирование на python\c3w4\test1_coursera_assignment_tmp-master\c3w4t1env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
